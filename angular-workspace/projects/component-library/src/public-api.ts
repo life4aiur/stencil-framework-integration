7fc698246ba6fdc21830141d0af05cd8355222ca
Andrew St. Jean <astjean@symplr.com>
2024-04-02 14:56:46 -0500

    chore: upgrade to Angular 14

--- angular-workspace/projects/component-library/src/public-api.ts
+++ angular-workspace/projects/component-library/src/public-api.ts
@@ -2,7 +2,5 @@
  * Public API Surface of component-library
  */
 
-export * from './lib/component-library.service';
-export * from './lib/component-library.component';
 export * from './lib/component-library.module';
 export * from './lib/stencil-generated/components';
