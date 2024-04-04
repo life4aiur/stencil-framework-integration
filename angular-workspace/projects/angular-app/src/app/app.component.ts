171d8496efd05b40cf02a048681857695b279e59
Andrew St. Jean <astjean@symplr.com>
2024-04-04 10:41:09 -0500

    chore: add form states

--- angular-workspace/projects/angular-app/src/app/app.component.ts
+++ angular-workspace/projects/angular-app/src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.myForm = this.fb.group({
-      longQuote: new UntypedFormControl(''),
+      myTextArea: new UntypedFormControl(''),
     });
   }
 
